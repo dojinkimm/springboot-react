14db171b2401f3b14662a630e86b2ec7fcfd9d9a
Henry <dojinkim119@gmail.com>
2019-05-19 17:20:21 +0900

    update minor changes

---
+++ src/main/resources/static/precache-manifest.4a3b5e9d92b818d203ea9e604250ae13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf74cee3b3861bbf7b3b27123b4e301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36b585cdb7b1f9b5aefe",
+    "url": "/static/css/2.4a9cee5a.chunk.css"
+  },
+  {
+    "revision": "587308c3e4b10a599f4b",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "36b585cdb7b1f9b5aefe",
+    "url": "/static/js/2.55264e90.chunk.js"
+  },
+  {
+    "revision": "587308c3e4b10a599f4b",
+    "url": "/static/js/main.6c71091c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
